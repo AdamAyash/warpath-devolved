104a0a0acaabc8f17bfdaf35f81cc52f735a9ce0
aayas <aayas@AAYASH>
2025-07-19 11:43:40 +0300

    Chnaged the projec structure and extracted every project into diffrent premake5 config file.

---
+++ dependencies.lua
@@ -0,0 +1,5 @@
+IncludeDir = {}
+IncludeDir["GLFW"] = "%{wks.location}/WCCEngine/vendor/GLFW/include"
+IncludeDir["Glad"] = "%{wks.location}/WCCEngine/vendor/Glad/include"
+IncludeDir["GLM"] = "%{wks.location}/WCCEngine/vendor/GLM/glm"
+IncludeDir["ImGUI"] = "%{wks.location}/WCCEngine/vendor/ImGUI"
--- premake5.lua
+++ premake5.lua
@@ -1,3 +1,5 @@
+include "dependencies.lua"
+
 workspace "WarcraftClone"
     architecture "x64"
     startproject "Sandbox"
@@ -11,170 +13,16 @@ workspace "WarcraftClone"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
-IncludeDir = {}
-IncludeDir["GLFW"] = "WCCEngine/vendor/GLFW/include"
-IncludeDir["Glad"] = "WCCEngine/vendor/Glad/include"
-IncludeDir["GLM"] = "WCCEngine/vendor/GLM/glm"
-IncludeDir["ImGUI"] = "WCCEngine/vendor/ImGUI"
+group "Dependencies"
+    include "WCCEngine/vendor/GLFW"
+    include "WCCEngine/vendor/Glad"
+    include "WCCEngine/vendor/ImGUI"
+group ""
 
-include "WCCEngine/vendor/GLFW"
-include "WCCEngine/vendor/Glad"
-include "WCCEngine/vendor/ImGUI"
+group "Core"
+	include "WCCEngine"
+group ""
 
-project "WCCEngine"
-    location "WCCEngine"
-    kind "SharedLib"
-    language "C++"
-    staticruntime "off"
-
-    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
-
-    pchheader "wccpch.h"
-    pchsource "WCCEngine/src/wccpch.cpp"
-
-    buildoptions
-    {
-        "/utf-8"
-    }
-
-    files
-    {
-        "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp"
-    }
-
-    includedirs
-    {
-        "%{prj.name}/vendor/spdlog/include",
-        "%{prj.name}/vendor/stb_image",
-        "%{IncludeDir.GLFW}",
-        "%{IncludeDir.Glad}",
-        "%{IncludeDir.GLM}",
-        "%{IncludeDir.ImGUI}",
-        "vendor/GLM/glm/**.hpp",
-		"vendor/GLM/glm/**.inl",
-    }
-
-    links
-    {
-        "GLFW",
-        "Glad",
-        "opengl32.lib",
-        "ImGUI"
-    }
-
-    filter "system:windows"
-        cppdialect "c++20"
-        staticruntime "off"
-        runtime "Debug"
-        systemversion "latest"
-
-        defines 
-        {
-            "WCC_BUILD_DLL",
-            "WCC_WINDOWS_PLATFORM"
-        }
-
-        postbuildcommands
-        {
-            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
-        }
-
-    filter "configurations:Debug"
-        defines "WCC_DEBUG"
-        symbols "On"
-
-    
-    filter "configurations:Release"
-        defines "WCC_RELEASE"
-        optimize "On"
-
-    
-    filter "configurations:Distribution"
-        defines "WCC_DISTRIBUTION"
-        optimize "On"
-    
-project "Sandbox"
-    location "Sandbox"
-    kind "ConsoleApp"
-    language "C++"
-
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-    buildoptions
-    {
-        "/utf-8"
-    }
-
-    files
-    {
-        "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp"
-    }
-
-    includedirs
-    {
-        "%{wks.location}/WCCEngine/vendor/spdlog/include",
-        "WCCEngine/src",
-        "%{IncludeDir.GLFW}",
-        "%{IncludeDir.Glad}",
-        "%{IncludeDir.GLM}",
-        "vendor/GLM/glm/**.hpp",
-		"vendor/GLM/glm/**.inl",
-    }
-
-    links
-    {
-        "WCCEngine"
-    }
-
-    filter "system:windows"
-        cppdialect "c++20"
-        staticruntime "On"
-        systemversion "latest"
-
-        defines 
-        {
-            "WCC_WINDOWS_PLATFORM"
-        }
-
-    filter "configurations:Debug"
-        defines "WCC_DEBUG"
-        symbols "On"
-
-    
-    filter "configurations:Release"
-        defines "WCC_RELEASE"
-        optimize "On"
-
-    
-    filter "configurations:Distribution"
-        defines "WCC_DISTRIBUTION"
-        optimize "On"
-    
-project "UnitTests"
-    location "UnitTests"
-    kind "SharedLib"
-    language "C++"
-
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-    files
-    {
-        "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp"
-    }
-
-    includedirs
-    {
-        "%{prj.name}/vendor/spdlog/include",
-        "WCCEngine/src"
-    }
-
-    links
-    {
-        "WCCEngine"
-    }
+group "Misc"
+	include "Sandbox"
+group ""
