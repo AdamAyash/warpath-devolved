6bd94bd1abc2653330d59e658a73910843cba0fe
Adam Ayash <aayash@mail.com>
2025-07-29 09:20:21 +0200

    Changes to the project setup scripts

--- scripts/LinuxSetup.py
+++ scripts/LinuxSetup.py
@@ -2,21 +2,17 @@ from Utilities import Loader
 import subprocess
 from pathlib import Path
 from urllib.request import urlretrieve
+import Globals
 import os
 
-PREMAKE5_DOWNLOAD_URL = ("https://github.com/premake/premake-core/releases/download/v5.0.0-beta7/premake-5.0.0-beta7-linux.tar.gz")
-VENDOR_FOLDER_RELATIVE_PATH = "vendor/bin/"
-PREMAKE5_ZIP_FILE_NAME = "premake.tar.gz"
-ANIMATION_TIMEOUT = 0.05
-
 def startLinuxSetup():
-    shouldDownloadPreamke = input("Do you wish to download \"Premake\"? y/n ")
-    if(shouldDownloadPreamke == "y" or shouldDownloadPreamke == "Y" or shouldDownloadPreamke == "yes"):
-        animationLoader = Loader("Generating project files... ", "Project setup is complete.", ANIMATION_TIMEOUT).start()
-        Path(VENDOR_FOLDER_RELATIVE_PATH).mkdir(parents=True, exist_ok=True)
-        urlretrieve(PREMAKE5_DOWNLOAD_URL, VENDOR_FOLDER_RELATIVE_PATH + PREMAKE5_ZIP_FILE_NAME)
-    elif(shouldDownloadPreamke != "n" and shouldDownloadPreamke != "no"):
+    shouldDownloadPremake = input("Do you wish to download \"Premake\"? y/n ")
+    if(shouldDownloadPremake == "y" or shouldDownloadPremake == "Y" or shouldDownloadPremake == "yes"):
+        animationLoader = Loader("Generating project files... ", "Project setup is complete.", Globals.ANIMATION_TIMEOUT).start()
+        Path(Globals.VENDOR_FOLDER_RELATIVE_PATH).mkdir(parents=True, exist_ok=True)
+        urlretrieve(Globals.PREMAKE5_LINUX_DOWNLOAD_URL, Globals.VENDOR_FOLDER_RELATIVE_PATH + Globals.PREMAKE5_ARCHIVE_FILE_NAME)
+    elif(shouldDownloadPremake != "n" and shouldDownloadPremake != "no"):
         print("Incorrect input.")
 
     # subprocess.call(["tar -x -f " + "../" + VENDOR_FOLDER_RELATIVE_PATH + PREMAKE5_ZIP_FILE_NAME])
-    subprocess.call([VENDOR_FOLDER_RELATIVE_PATH +" premake gmake"])
+    subprocess.call([Globals.VENDOR_FOLDER_RELATIVE_PATH +" premake gmake"])
--- scripts/WindowsSetup.py
+++ scripts/WindowsSetup.py
@@ -4,25 +4,24 @@ from pathlib import Path
 from urllib.request import urlretrieve
 from Utilities import Loader
 import os
-
-PREMAKE5_DOWNLOAD_URL = ("https://github.com/premake/premake-core/releases/download/v5.0.0-beta6/premake-5.0.0-beta6-windows.zip")
-PREMAKE5_ZIP_FILE_NAME = "premake.zip"
-VENDOR_FOLDER_RELATIVE_PATH = "/vendor/bin"
-ANIMATION_TIMEOUT = 0.05
+import Globals
 
 def startWindowsSetup():
-    shouldDownloadPreamke = input("Do you wish to download \"Premake\"? y/n ")
-    if(shouldDownloadPreamke == "y" or shouldDownloadPreamke == "Y" or shouldDownloadPreamke == "yes"):
-        animationLoader = Loader("Generating project files... ", "Project setup is complete.", ANIMATION_TIMEOUT).start()
-        Path(VENDOR_FOLDER_RELATIVE_PATH).mkdir(parents=True, exist_ok=True)
-        urlretrieve(PREMAKE5_DOWNLOAD_URL, PREMAKE5_ZIP_FILE_NAME)
-    elif(shouldDownloadPreamke != "n" and shouldDownloadPreamke != "no"):
-        print("Incorrect input.")
-    
-    with zipfile.ZipFile(PREMAKE5_ZIP_FILE_NAME, 'r') as zipFileReference:
-        zipFileReference.extractall(VENDOR_FOLDER_RELATIVE_PATH)
-     
-    os.remove(PREMAKE5_ZIP_FILE_NAME)
-    subprocess.call(["/vendor/bin/premake5.exe", "vs2022"])
-    animationLoader.stop()
-    print("WCC Engine setup has completed.")
+     try:
+      shouldDownloadPremake = input("Do you wish to download \"Premake\"? y/n ")
+      if(shouldDownloadPremake == "y" or shouldDownloadPremake == "Y" or shouldDownloadPremake == "yes"):
+          animationLoader = Loader("Generating project files... ", "Project setup is complete.", Globals.ANIMATION_TIMEOUT).start()
+          Path(Globals.VENDOR_FOLDER_RELATIVE_PATH).mkdir(parents=True, exist_ok=True)
+          urlretrieve(Globals.PREMAKE5_WINDOWS_DOWNLOAD_URL, Globals.PREMAKE5_ZIP_FILE_NAME)
+      elif(shouldDownloadPremake != "n" and shouldDownloadPremake != "no"):
+          print("Incorrect input.")
+  
+      with zipfile.ZipFile(Globals.PREMAKE5_ZIP_FILE_NAME, 'r') as zipFileReference:
+          zipFileReference.extractall(Globals.VENDOR_FOLDER_RELATIVE_PATH)
+  
+      os.remove(Globals.PREMAKE5_ZIP_FILE_NAME)
+      subprocess.call(["/vendor/bin/premake5.exe", "vs2022"])
+      animationLoader.stop()
+      print("WCC Engine setup has completed.")
+     except:
+         print("An error occurred while trying to generate the project. Please, try again.")
